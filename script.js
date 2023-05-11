3aad9e7880f455f97d67077c8b31b4bf2b1bd9c2
DuckiDeluxe <93772076+DuckiDeluxe@users.noreply.github.com>
2023-05-11 19:38:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -921,29 +921,4 @@ new Vue({
       }
     }, 400)
   },
-  methods: {
 
-    toggleGo() {
-      this.go = !this.go;
-      if(this.go) this.vibrate();
-    },
-
-    vibrate() {
-      this.gamepad.vibrationActuator.playEffect('dual-rumble', {
-        duration: 100,
-        strongMagnitude: this.magnitude/100,
-        weakMagnitude: this.magnitude/100
-      })
-        .then(() => {
-          if(this.go) this.vibrate();
-        })
-        .catch(err => console.log(err))
-    }
-  },
-  computed: {
-
-    button() {
-      return this.go ? 'Stop' : 'Go'
-    }
-  }
-});
